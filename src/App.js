e3df8b0e2c2f521935ba6bb58a12c657ae9d1037
JayEee2U <j_double_u@icloud.com>
2024-03-27 22:46:23 -0400

    Update App.js  commenting out Route paths

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/HerLife" element={<HerLife />} />
         <Route path="/Gallery" element={<Gallery />} />
         <Route path="/TreasuredStories" element={<TreasuredStories />} />
-        <Route path="/*" element={<PageNotFound />} />
+        <Route path="/*" element={<PageNotFound />} /> */}
       </Routes>
     </BrowserRouter>
   );
