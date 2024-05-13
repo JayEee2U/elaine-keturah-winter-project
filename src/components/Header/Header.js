40b298cbfa1afa8a433da4a8f9fc6387bfedcf00
JayEee2U <j_double_u@icloud.com>
2024-05-13 11:46:15 -0400

    added buttons, img attribute

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,18 @@
 import "./Header.scss";
 
 function Header() {
-  return <></>;
+  return (
+    <header>
+      <div className="button">
+        <img className="logo" src=""></img>
+        <button>Home</button>
+        <button>About</button>
+        <button>HerLife</button>
+        <button>Photo Gallery</button>
+        <button>TreasuredStories</button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
