7c4721f20246355843c66e481c6fedc07c64d7ca
JayEee2U <j_double_u@icloud.com>
2024-05-13 11:50:49 -0400

    removed routes at this time

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/HerLife" element={<HerLife />} />
-        <Route path="/Gallery" element={<Gallery />} />
-        <Route path="/TreasuredStories" element={<TreasuredStories />} />
-        <Route path="/*" element={<PageNotFound />} /> */}
-      </Routes>
     </BrowserRouter>
   );
 }
